c23535de32786fd249566bbc95e3ab74146cff48
haseeb ashraf <haseeb.ashraf@psca.gop.pk>
2025-04-18 12:03:46 +0500

    dashboard tile section is completed

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -7,114 +7,114 @@ import SettingsIcon from "@mui/icons-material/Settings";
 import LogoutIcon from '@mui/icons-material/Logout';
 
 const Sidebar = () => {
-const [openMenus, setOpenMenus] = useState({});
+  const [openMenus, setOpenMenus] = useState({});
 
-// const toggleMenu = (menuName) => {
-// setOpenMenus((prev) => ({
-// ...prev,
-// [menuName]: !prev[menuName],
-// }));
-// }; can open all submenu in the same time
-
-
-const toggleMenu = (menuName) => {
-setOpenMenus((prev) => {
-// If the same menu is clicked again, toggle it off
-if (prev[menuName]) return {};
-// Else, close all and open the clicked one
-return { [menuName]: true };
-});
-};
+  // const toggleMenu = (menuName) => {
+  // setOpenMenus((prev) => ({
+  // ...prev,
+  // [menuName]: !prev[menuName],
+  // }));
+  // }; can open all submenu in the same time
 
 
-return (
-<div className="sidebar">
-<ul>
-<li>
-<Button className="w-100">
-<span className="icon">
-<DashboardIcon />
-</span>
-Dashboard
-</Button>
-</li>
-<li>
-<Button className="w-100">
-<span className="icon">
-<DashboardIcon />
-</span>
-Cases
-</Button>
-</li>
+  const toggleMenu = (menuName) => {
+    setOpenMenus((prev) => {
+      // If the same menu is clicked again, toggle it off
+      if (prev[menuName]) return {};
+      // Else, close all and open the clicked one
+      return { [menuName]: true };
+    });
+  };
 
-{/* Submenu 1 */}
-<li>
-<Button className="w-100" onClick={() => toggleMenu("complaints")}>
-<span className="icon">
-<DashboardIcon />
-</span>
-User Complaints
-<span
-className={`arrow ${openMenus["complaints"] ? "rotate" : ""}`}
->
-<KeyboardArrowRightIcon />
-</span>
-</Button>
-<Collapse in={openMenus["complaints"]}>
-<div className="submenuWrapper">
-<ul className="submenu">
-<li>User 1</li>
-<li>User 2</li>
-<li>User 3</li>
-</ul>
-</div>
-</Collapse>
-</li>
 
-{/* Submenu 2 */}
-<li>
-<Button className="w-100" onClick={() => toggleMenu("permissions")}>
-<span className="icon">
-<DashboardIcon />
-</span>
-Permission
-<span
-className={`arrow ${openMenus["permissions"] ? "rotate" : ""}`}
->
-<KeyboardArrowRightIcon />
-</span>
-</Button>
-<Collapse in={openMenus["permissions"]}>
-<div className="submenuWrapper">
-<ul className="submenu">
-<li>Role 1</li>
-<li>Role 2</li>
-</ul>
-</div>
-</Collapse>
-</li>
+  return (
+    <div className="sidebar">
+      <ul>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <DashboardIcon />
+            </span>
+            Dashboard
+          </Button>
+        </li>
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <DashboardIcon />
+            </span>
+            Cases
+          </Button>
+        </li>
 
-<li>
-<Button className="w-100">
-<span className="icon">
-<SettingsIcon />
-</span>
-Settings
-</Button>
-</li>
-</ul>
-<br />
-<div class="logoutWrapper">
-<div class="logoutBox">
-<Button className="gap-2">
-<LogoutIcon fontSize="small"/>
-<span>Logout</span>
-</Button>
-</div>
-</div>
+        {/* Submenu 1 */}
+        <li>
+          <Button className="w-100" onClick={() => toggleMenu("complaints")}>
+            <span className="icon">
+              <DashboardIcon />
+            </span>
+            User Complaints
+            <span
+              className={`arrow ${openMenus["complaints"] ? "rotate" : ""}`}
+            >
+              <KeyboardArrowRightIcon />
+            </span>
+          </Button>
+          <Collapse in={openMenus["complaints"]}>
+            <div className="submenuWrapper">
+              <ul className="submenu">
+                <li>User 1</li>
+                <li>User 2</li>
+                <li>User 3</li>
+              </ul>
+            </div>
+          </Collapse>
+        </li>
 
-</div>
-);
+        {/* Submenu 2 */}
+        <li>
+          <Button className="w-100" onClick={() => toggleMenu("permissions")}>
+            <span className="icon">
+              <DashboardIcon />
+            </span>
+            Permission
+            <span
+              className={`arrow ${openMenus["permissions"] ? "rotate" : ""}`}
+            >
+              <KeyboardArrowRightIcon />
+            </span>
+          </Button>
+          <Collapse in={openMenus["permissions"]}>
+            <div className="submenuWrapper">
+              <ul className="submenu">
+                <li>Role 1</li>
+                <li>Role 2</li>
+              </ul>
+            </div>
+          </Collapse>
+        </li>
+
+        <li>
+          <Button className="w-100">
+            <span className="icon">
+              <SettingsIcon />
+            </span>
+            Settings
+          </Button>
+        </li>
+      </ul>
+      <br />
+      <div class="logoutWrapper">
+        <div class="logoutBox">
+          <Button className="gap-2">
+            <LogoutIcon fontSize="small" />
+            <span>Logout</span>
+          </Button>
+        </div>
+      </div>
+
+    </div>
+  );
 };
 
 export default Sidebar;
--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,9 +1,240 @@
 import React from 'react'
+import Button from '@mui/material/Button';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import MovingIcon from '@mui/icons-material/Moving';
 
-const index = () => {
+
+
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { Chart } from "react-google-charts";
+
+
+const Dashboard = () => {
+
+
+  const data = [
+    ["Jan", "Hours per Day"],
+    ["Jan", 9],
+    ["Feb", 9],
+    ["March", 8],
+   
+  ];
+  const options = {
+    titleTextStyle:{color:"white"},
+    title: "Recemt Profit Stats",
+    backgroundColor:"transparent",
+    chartArea:{'width':'100%','height':'80%'}
+  };
+
+
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+
   return (
-    <h3>Page section</h3>
+    <>
+
+      {/* Start sesction 1 */}
+      <section className='userTileSection'>
+        <div className="container-fluid gx-0">
+          <div className="row">
+            <div className=" col-sm-12 col-md-8">
+              <div className="userTileContainer d-flex flex-wrap gap-2 justify-content-between">
+                <Button className="tile tile-green d-block">
+                  <div className="d-flex justify-content-between">
+                    <div className="col1">
+                      <h6 className='m-0'>Total Users</h6>
+                      <span>277</span>
+                    </div>
+                    <div className="iconContainer">
+                      <span><AccountCircleIcon /></span>
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-between">
+                    <span className='lastMonth'>Last Month</span>
+                    <div className="icon-container">
+                      <Button >
+                        <MoreVertIcon onClick={handleClick} />
+                        <Menu
+                          id="fade-menu"
+                          anchorEl={anchorEl}
+                          open={open}
+                          onClose={handleClose}
+                        >
+                          <MenuItem onClick={handleClose}>Last Day</MenuItem>
+                          <MenuItem onClick={handleClose}>Last Week</MenuItem>
+                          <MenuItem onClick={handleClose}>Last Month</MenuItem>
+                        </Menu>
+                      </Button>
+                    </div>
+                  </div>
+                  <div className="movingIconContainer">
+                    <MovingIcon />
+                  </div>
+
+                </Button>
+
+                <Button className="tile tile-pink d-block">
+                  <div className="d-flex justify-content-between">
+                    <div className="col1">
+                      <h6 className='m-0'>Total Users</h6>
+                      <span>277</span>
+                    </div>
+                    <div className="iconContainer">
+                      <span><AccountCircleIcon /></span>
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-between">
+                    <span className='lastMonth'>Last Month</span>
+                    <div className="icon-container">
+                      <Button >
+                        <MoreVertIcon onClick={handleClick} />
+                        <Menu
+                          id="fade-menu"
+                          anchorEl={anchorEl}
+                          open={open}
+                          onClose={handleClose}
+                        >
+                          <MenuItem onClick={handleClose}>Last Day</MenuItem>
+                          <MenuItem onClick={handleClose}>Last Week</MenuItem>
+                          <MenuItem onClick={handleClose}>Last Month</MenuItem>
+                        </Menu>
+                      </Button>
+                    </div>
+                  </div>
+                  <div className="movingIconContainer">
+                    <MovingIcon />
+                  </div>
+
+                </Button>
+
+                <Button className="tile tile-blue d-block">
+                  <div className="d-flex justify-content-between">
+                    <div className="col1">
+                      <h6 className='m-0'>Total Users</h6>
+                      <span>277</span>
+                    </div>
+                    <div className="iconContainer">
+                      <span><AccountCircleIcon /></span>
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-between">
+                    <span className='lastMonth'>Last Month</span>
+                    <div className="icon-container">
+                      <Button >
+                        <MoreVertIcon onClick={handleClick} />
+                        <Menu
+                          id="fade-menu"
+                          anchorEl={anchorEl}
+                          open={open}
+                          onClose={handleClose}
+                        >
+                          <MenuItem onClick={handleClose}>Last Day</MenuItem>
+                          <MenuItem onClick={handleClose}>Last Week</MenuItem>
+                          <MenuItem onClick={handleClose}>Last Month</MenuItem>
+                        </Menu>
+                      </Button>
+                    </div>
+                  </div>
+                  <div className="movingIconContainer">
+                    <MovingIcon />
+                  </div>
+
+                </Button>
+
+                <Button className="tile tile-orange d-block">
+                  <div className="d-flex justify-content-between">
+                    <div className="col1">
+                      <h6 className='m-0'>Total Users</h6>
+                      <span>277</span>
+                    </div>
+                    <div className="iconContainer">
+                      <span><AccountCircleIcon /></span>
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-between">
+                    <span className='lastMonth'>Last Month</span>
+                    <div className="icon-container">
+                      <Button >
+                        <MoreVertIcon onClick={handleClick} />
+                        <Menu
+                          id="fade-menu"
+                          anchorEl={anchorEl}
+                          open={open}
+                          onClose={handleClose}
+                        >
+                          <MenuItem onClick={handleClose}>Last Day</MenuItem>
+                          <MenuItem onClick={handleClose}>Last Week</MenuItem>
+                          <MenuItem onClick={handleClose}>Last Month</MenuItem>
+                        </Menu>
+                      </Button>
+                    </div>
+                  </div>
+                  <div className="movingIconContainer">
+                    <MovingIcon />
+                  </div>
+
+                </Button>
+
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="graphBox">
+                <div className="d-flex justify-content-between">
+                  <span className='lastMonth'>Total Sales</span>
+                  <div className="icon-container">
+                    <Button >
+                      <MoreVertIcon onClick={handleClick} />
+                      <Menu
+                        id="fade-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                      >
+                        <MenuItem onClick={handleClose}>Last Day</MenuItem>
+                        <MenuItem onClick={handleClose}>Last Week</MenuItem>
+                        <MenuItem onClick={handleClose}>Last Month</MenuItem>
+                      </Menu>
+                    </Button>
+                  </div>
+                </div>
+                <p className='totalSales'>$3,787,681.00</p>
+                <p className='lastMonthSale'>$3,578.90 in last month</p>
+
+                <div className="chartContainer mt-4">
+
+                  <Chart
+                    chartType="PieChart"
+                    data={data}
+                    options={options}
+                    width={"100%"}
+                    height={"200px"}
+                  />
+
+                </div>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+
+      </section>
+      {/* End sesction 1 */}
+
+    </>
   )
 }
 
-export default index 
+export default Dashboard  
